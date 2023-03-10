91c2b08f33b44c51dc35a47406bc7eb06fb41e5b
user <assassinmaks2007@gmail.com>
2023-03-10 15:14:41 +0900

    1231

--- data/jobs.py
+++ data/jobs.py
@@ -1,7 +1,8 @@
 import datetime
 import sqlalchemy
-from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
 
 
 class Jobs(SqlAlchemyBase):
@@ -9,10 +10,10 @@ class Jobs(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
-    user = orm.relationship('User')
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("jobs.id"))
+    user = orm.relationship('Jobs')
--- main.py
+++ main.py
@@ -1,51 +1,79 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
-from data.users import User
 from data.jobs import Jobs
-from data.register import RegisterForm
+from data.users import User
+from flask_login import LoginManager, login_user, login_required, logout_user
+from forms.login_form import LoginForm
+from forms.register_form import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/')
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    app.run(port=8000, host='127.0.0.1')
+#    app.run("localhost", "5050")
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+@app.route("/")
 def index():
     session = db_session.create_session()
     jobs = session.query(Jobs).all()
     users = session.query(User).all()
-    names = {name.id: (name.surname, name.name) for name in users}
+    names = {name.id: name.name for name in users}
     return render_template("index.html", jobs=jobs, names=names)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация', form=form,
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
                                    message="Пароли не совпадают")
-        session = db_session.create_session()
-        if session.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация', form=form,
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
                                    message="Такой пользователь уже есть")
         user = User(
-            surname=form.surname.data,
             name=form.name.data,
-            age=form.age.data,
-            position=form.position.data,
-            speciality=form.speciality.data,
-            address=form.address.data,
-            email=form.email.data)
+            email=form.email.data,
+            about=form.about.data
+        )
         user.set_password(form.password.data)
-        session.add(user)
-        session.commit()
-        #return redirect('/login')
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
-
-
-def main():
-    db_session.global_init("db/mars_explorer.db")
-    app.run()
 
 
 if __name__ == '__main__':
